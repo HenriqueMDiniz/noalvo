aba9f449e1af75c27dc99d1ea3117b87af85711f
HenriqueMDiniz <91998661+HenriqueMDiniz@users.noreply.github.com>
2021-11-29 23:11:54 -0300

    Update cadastro.php

--- site noalvo/cadastro.php
+++ site noalvo/cadastro.php
@@ -1,7 +1,50 @@
 <?php 
+
+//1 – Definimos Para quem vai ser enviado o email
+$para = "seuemail@algumacoisa.com.br";
+//2 - resgatar o nome digitado no formulário e  grava na variavel $nome
+$nome = $_POST ["nome"]; 
+$perfil = $_POST ["perfil"]; 
+$endereco = $_POST ["endereco"]; 
+$bairro = $_POST ["bairro"]; 
+$telefone = $_POST ["telefone"]; 
+$email = $_POST ["email"]; 
+$servico = $_POST ["servico"]; 
+$clm = $_POST ["clm"]; 
+$descricao = $_POST ["descricao"]; 
+$data = date('d/m/Y'); 
+$ip = $_SERVER["REMOTE_ADDR"]; 
+// 3 - resgatar o assunto digitado no formulário e  grava na variavel
+ //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
+$msg = "Estes são os dados de seu formulário. Eles foram enviados por:<br>";
+
+if($ip != is_null()) $msg .= "Ip:  $ip <br>";
+if($data != is_null()) $msg .= "Data: $data";
+if($nome != is_null()) $msg .= "Nome: $nome <br>";
+if($telefone != is_null()) $msg .= "Telefone: $telefone <br>";
+if($email != is_null()) $msg .= "Email: $email <br>";
+if($endereco != is_null()) $msg .= "endereco: $endereco<br>";
+if($bairro != is_null()) $msg .= "Bairro: $bairro<br>";
+if($perfil != is_null()) $msg .= "Perfil: $perfil <br>";
+if($servico != is_null()) $msg .= "Serviço: $servico <br>";
+if($clm != is_null()) $msg .= "CLM: $clm <br>";
+if($descricao != is_null()) $msg .= "descrição: $descricao <br>";
+
+//5 – agora inserimos as codificações corretas e  tudo mais.
+$headers =  "Content-Type:text/html; charset=UTF-8\n";
+$headers .= "From:  site NOALVO TELECOM <site@noalvotelecom.com.br>\n";
+//Vai ser //mostrado que  o email partiu deste email e seguido do nome
+$headers .= "X-Sender:  <contato@noalvotelecom.com.br>\n";
+//email do servidor //que enviou
+$headers .= "X-Mailer: PHP  v".phpversion()."\n";
+$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
+$headers .= "Return-Path:  <$email>\n";
+//caso a msg //seja respondida vai para  este email.
+$headers .= "MIME-Version: 1.0\n";
+
+$envio = mail($para, $assunto, $msg, $headers);  //função que faz o envio do email.
+
 //PREENCHA OS DADOS DE CONEXÃO A SEGUIR: 
-$host= 'localhost'; 
-$bd= 'orcamento'; 
 $senhabd= ''; 
 $userbd = 'root'; 
 // RECEBENDO OS DADOS PREENCHIDOS DO FORMULÁRIO ! 
